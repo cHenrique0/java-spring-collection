54599218103ad4caf76d747d42c780e26410a809
Claudio Henrique <claudio_henrique20@hotmail.com>
2025-08-09 20:56:56 -0300

    feat: add terminal log

--- 1-professional/1.1-componentes-injecao-dependencia/desafio/src/main/java/professional/spring/course/desafio/DesafioApplication.java
+++ 1-professional/1.1-componentes-injecao-dependencia/desafio/src/main/java/professional/spring/course/desafio/DesafioApplication.java
@@ -1,13 +1,29 @@
 package professional.spring.course.desafio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import professional.spring.course.desafio.entities.Order;
+import professional.spring.course.desafio.services.OrderService;
 
 @SpringBootApplication
-public class DesafioApplication {
+public class DesafioApplication implements CommandLineRunner {
+
+	@Autowired
+	OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Order order = new Order(1034, 150.0, 20.0);
+
+		Double totalOrder = orderService.total(order);
+
+		System.out.printf("%nPedido código %d%nValor total: R$ %.2f%n", order.getCode(), totalOrder);
+	}
 }
