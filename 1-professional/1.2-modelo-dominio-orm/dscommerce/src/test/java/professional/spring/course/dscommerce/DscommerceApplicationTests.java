e60b9f254e234f93bbece1af9d02be0f33ef47e2
Claudio Henrique <claudio_henrique20@hotmail.com>
2025-08-16 16:09:34 -0300

    init dscommerce

---
+++ 1-professional/1.2-modelo-dominio-orm/dscommerce/src/test/java/professional/spring/course/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package professional.spring.course.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
