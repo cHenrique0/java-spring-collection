7f57fdb171e69a1339b6f28ad314b74a37741b4c
Claudio Henrique <claudio_henrique20@hotmail.com>
2025-08-17 10:53:34 -0300

    chore: add get orders and products

--- 1-professional/1.2-modelo-dominio-orm/dscommerce/src/main/java/professional/spring/course/dscommerce/entities/Order.java
+++ 1-professional/1.2-modelo-dominio-orm/dscommerce/src/main/java/professional/spring/course/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package professional.spring.course.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -16,9 +19,10 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
-
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {}
 
@@ -68,5 +72,13 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(item -> item.getProduct()).toList();
     }
 }
