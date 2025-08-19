49e519c0a9b7c3b2fa0b8796bea5baa61784a744
Claudio Henrique <claudio_henrique20@hotmail.com>
2025-08-19 20:44:58 -0300

    feat: add product service

---
+++ 1-professional/dscommerce/src/main/java/professional/spring/course/dscommerce/services/ProductService.java
@@ -0,0 +1,21 @@
+package professional.spring.course.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import professional.spring.course.dscommerce.dto.ProductDTO;
+import professional.spring.course.dscommerce.entities.Product;
+import professional.spring.course.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = productRepository.findById(id).get();
+        return new ProductDTO(product);
+    }
+}
