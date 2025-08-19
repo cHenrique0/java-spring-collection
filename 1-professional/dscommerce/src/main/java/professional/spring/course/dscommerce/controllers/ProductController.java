6b393bd2b49adb8290ca936c94f86ed13e05de21
Claudio Henrique <claudio_henrique20@hotmail.com>
2025-08-19 20:45:24 -0300

    refactor: product controller

--- 1-professional/dscommerce/src/main/java/professional/spring/course/dscommerce/controllers/ProductController.java
+++ 1-professional/dscommerce/src/main/java/professional/spring/course/dscommerce/controllers/ProductController.java
@@ -1,15 +1,22 @@
 package professional.spring.course.dscommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import professional.spring.course.dscommerce.dto.ProductDTO;
+import professional.spring.course.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
-    @GetMapping
-    public String test() {
-        return "Olá, Mundo!";
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return productService.findById(id);
     }
 }
