0d7b8ee10ceec2c72490e85704db82dcecc1d6c1
Claudio Henrique <claudio_henrique20@hotmail.com>
2025-08-19 14:07:55 -0300

    feat: add module 1.2 challenge

---
+++ 1-professional/1.2-modelo-dominio-orm/desafio-modelo-dominio/README.md
@@ -0,0 +1,22 @@
+# DESAFIO: Componentes e injeção de dependência
+
+## 📋 Descrição
+Criar um projeto no `Spring Boot` com `Java` e banco de dados `H2`, e implementar o modelo
+conceitual conforme a especificação.
+Para mais detalhes acesse as [instruções do desafio](https://github.com/cHenrique0/java-spring-collection/blob/730b3691ef7633cf2b9d37f31624f84458745ecd/1-professional/material-apoio/02_DESAFIO_Modelo_de_dominio_e_ORM.pdf).
+
+## 🛠️ Tecnologias Utilizadas
+- Java 21
+- Spring Boot 3.5.4
+- Maven 3.8.7
+- H2 database
+
+## 🚀 Como Executar
+### Executando via terminal
+```bash
+git clone [repo-url]
+cd nome-do-projeto
+mvn spring-boot:run
+```
+
+> Você pode executar na sua IDE de preferência.
---
+++ 1-professional/1.2-modelo-dominio-orm/desafio-modelo-dominio/src/main/resources/import.sql
@@ -0,0 +1,26 @@
+-- Criando participantes
+INSERT INTO tb_participante(nome, email) VALUES ('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante(nome, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante(nome, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante(nome, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
+
+-- Criando categorias
+INSERT INTO tb_categoria(descricao) VALUES ('Curso');
+INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
+
+-- Criando atividades
+INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.0, 1);
+INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.0, 2);
+
+-- Criando blocos
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z', 1);
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z', 2);
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z', 2);
+
+-- Associando atividades e participantes
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (1, 1);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (1, 2);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (1, 3);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2, 1);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2, 3);
+INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2, 4);
